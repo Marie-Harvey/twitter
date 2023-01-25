1d1afb906deaac3c65fa3518a1543e30e6f589f9
mdkwe <michelle.dkwe@gmail.com>
2023-01-25 18:54:27 +0100

    Version ok / À améliorer test

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,11 @@
   # ligne très importante qui appelle la gem.
   require 'twitter'
-
   require 'dotenv'
-  Dotenv.load('../.env')
+  # require 'pry'
 
   # n'oublie pas les lignes pour Dotenv ici…
-  
+  Dotenv.load
+
   # quelques lignes qui appellent les clés d'API de ton fichier .env
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
@@ -13,6 +13,8 @@
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end
+  
+  # binding.pry
   
   # ligne qui permet de tweeter sur ton compte
   client.update('Mon premier tweet en Ruby !!!!')
